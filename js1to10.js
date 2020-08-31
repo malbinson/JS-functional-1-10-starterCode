4d7c0b3eec1ffdab76c5907e7615b8e11b1ef7da
Matt Albinson <mattalbinson@berkeley.net>
2020-08-31 18:30:37 +0000

    Initial commit

---
+++ js1to10.js
@@ -0,0 +1,12 @@
+//complete 12 problems here and use the "Share" button
+//to share this repl.it back with Albinson
+
+function all_even(x){
+  return [-2,4,42];
+}
+
+function no_space(x){
+  return ["hi","foo"];
+}
+
+
